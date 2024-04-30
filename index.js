e6d6e16d14be22f479483ccde1986f4b77c21bc5
Sayem <mustakhasansayem@gmail.com>
2024-04-30 19:34:13 +0600

    First version Done

--- index.js
+++ index.js
@@ -102,15 +102,10 @@ async function run() {
       res.send(result)
   })
 
-
-
-
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
    
-    // await client.close();
   }
 }
 run().catch(console.dir);
